507168411c3f01680c39d04dd04b2417f290373b
takeItIzzy <q1367257497@gmail.com>
2021-12-01 21:05:24 +0800

    feat(content component): add className prop

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { IComponentBaseProps } from '@/components/index.types';
 
 const Content: React.FC<IComponentBaseProps> = (props) => {
-  const { children } = props;
-  return <div>{children}</div>;
+  const { children, className } = props;
+  return <div className={className}>{children}</div>;
 };
 
 export default Content;
